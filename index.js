deee27d183f8e3576c7435308cf713011cec0b40
Ivan Chelyadin <hacker.assasin@yandex.ru>
2019-10-19 15:18:34 +0300

    Подготовка сервера к приему JSON объектов

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
 const debugPath = require("./routes/debug");
 const googleAuthPath = require("./routes/google");
 const mongoose = require("mongoose");
@@ -13,6 +14,7 @@ mongoose.connect(dbURI, {useUnifiedTopology: true,
 .catch((err) => console.log(err));
 
 
+app.use(bodyParser.json());
 
 app.get("/",function(req,res) {
     res.send("Hello, World!");
--- routes/login.js
+++ routes/login.js
@@ -6,12 +6,12 @@ module.exports.login = function(req, res) {
     console.log(req.headers);
     userModel.findOne({mail:req.query.mail, pass:req.query.pass}, (err, doc) => {
         if (err) throw err;
-        if (!doc) res.send("Wrong credentials"); 
+        if (!doc) res.json({status:400,msg:"Wrong credentials"}); 
         else {
             console.log(doc);
             var payload = {type:"local",id:doc._id};
             var token = jwt.sign(payload,process.env.SECRET_KEY);
-            res.send(token);
+            res.json({status:200,token:token});
         };
     })
 }
